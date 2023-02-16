77d277775d2adb6f3f0e3c49984f6d2c0050e65d
Onakleaver <kleaver49@gmail.com>
2023-02-16 18:02:49 +0000

    Use of puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # include <stdio.h>
 
 /**
@@ -8,6 +9,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
