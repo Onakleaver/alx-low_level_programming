2586bba17847562f76fb000ca08133bb56536e8c
Onakleaver <kleaver49@gmail.com>
2023-02-17 19:16:27 +0000

    Write a script with a loop

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int n;
+
+	while (n = rand) 
+	{
+		printf("value of n: %d\n", n);
+		a++;
+	}
+
+	return (0);
+}
