39878a2a306621ea9b39409e45cace373b8f56f5
Onakleaver <kleaver49@gmail.com>
2023-02-18 12:39:36 +0000

    Small letters of the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print the lphabet in lower case
+ *Return: Always 0
+ */
+main()
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('/n');
+
+	return (0);
+}
