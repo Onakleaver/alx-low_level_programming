b487647ab1ef0413cf02e3d7623f8d20842a4ab1
Onakleaver <kleaver49@gmail.com>
2023-02-18 11:44:12 +0000

    Writing a C script using if and else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,13 +6,11 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+	int n, a;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int a;
-
 	a = n % 10;
 
 	printf("Last digit of %d is ", a);
