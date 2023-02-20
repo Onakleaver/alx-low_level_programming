2d3f8bd29e56a178a6a1fb0dbd0c3eff16827ec3
Onakleaver <kleaver49@gmail.com>
2023-02-19 17:41:44 -0800

    1 Lat digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
 /* more headers goes there */
 /* betty style doc for function main goes there*/
+/**
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int n, a;
@@ -18,7 +24,7 @@ int main(void)
 	}
 	else if (a == 0)
 	{
-		printf("%d and is 0\n",n a);
+		printf("%d and is 0\n", a);
 	}
 	else
 	{
