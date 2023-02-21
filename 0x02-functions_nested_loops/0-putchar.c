b5f4128d1645d5f5609213ea0cbd32372c6b9d5b
Onakleaver <kleaver49@gmail.com>
2023-02-21 18:41:36 +0000

    Print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
  * main - prints _putchar followed by a new line.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	putchar("_putchar,\n");
