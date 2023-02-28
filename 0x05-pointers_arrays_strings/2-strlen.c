8ec2c89a2da3f6fc746f7cbd0478418cf9698e49
Onakleaver <kleaver49@gmail.com>
2023-02-28 17:46:22 +0000

    Find the lenth of the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - returns the lenth of a string
+ * @s - character to test
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	strlen(s);
+}
