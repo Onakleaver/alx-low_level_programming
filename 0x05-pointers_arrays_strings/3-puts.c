799c99366664278f38041d52867ebd5c45f8b054
Onakleaver <kleaver49@gmail.com>
2023-03-01 02:10:39 +0000

    Add a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - print string to stdout
+ * @str: arry string type
+ */
+
+void _puts(char *str)
+{
+	int b;
+
+	for (b = 0; str[b] != '\0'; b++)
+	{
+		_putchar(str[b]);
+	}
+
+	_putchar('\b');
+}
