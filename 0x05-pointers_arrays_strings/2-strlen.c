0331ff928b4763936578907804820cd069eba323
Onakleaver <kleaver49@gmail.com>
2023-03-01 01:57:29 +0000

    Find the lenth of strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,13 +5,16 @@
 /**
  * _strlen - returns the lenth of a string
  * @s: character type pointer
+ *
  * Return: lenth of string
  */
+
 int _strlen(char *s)
 {
 	int a;
 
-	for (a = 0; s[a] != '\0'; a++);
+	for (a = 0; s[a] != '\0'; a++)
+		;
 
 	return (a);
 }
