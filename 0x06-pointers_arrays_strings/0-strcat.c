be9d42890b9f8832347229a9d52841abc73f3e55
Onakleaver <kleaver49@gmail.com>
2023-03-02 02:19:50 +0000

    Concartinate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcat - concartinates two strings
+ * @dest: test destination string
+ * @src: test source string
+ *
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	dest = 'n';
+	src = 'i';
+
+	_strcat(dest, src);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+This project handles more arrays and pointers in C.
+Concartinating strings
+Coping strings
+Comparing strings
+Changing Cases of strings
