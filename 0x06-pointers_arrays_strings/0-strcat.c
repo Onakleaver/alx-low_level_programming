402b6bad88f30c5881a87d8f5ad74990e72837eb
Onakleaver <kleaver49@gmail.com>
2023-03-02 15:56:50 +0000

    Concartinate the strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,19 +10,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int j = 0, i = 0;
+	int i = 0, j = 0;
 
-	while (dest[j] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while (src[i] != '\0')
+	while (src[j] != '\0')
 	{
-		dest[j] = src[i];
+		dest[i] = src[j];
+		i++;
 		j++;
-		i++;
 	}
-	dest[j] = '\0';
+	dest[i] = '\0';
 
-	return 0;
+	return dest;
 }
