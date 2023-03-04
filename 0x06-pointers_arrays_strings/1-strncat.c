9c290fc4110f4dd1db9e4b764b3e6d8e7319e7b9
Onakleaver <kleaver49@gmail.com>
2023-03-04 02:08:04 +0000

    Concartinate string to specific character

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concartinates two strings
+ * @dest: test destination string
+ * @src: test source string
+ *@n: number of bytes src can store
+ * Return: Always 0.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	size_t dest_len = strlen(dest);
+	size_t i;
+
+	size_t n;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	
+	dest[dest_len + i] = src[i];
+
+	return (dest);
+}
