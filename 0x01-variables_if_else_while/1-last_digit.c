b325589aaf120aafd80def1b1c722cc8c672733f
Kleaver <KLeaver49@gmail.com>
2023-06-16 22:04:45 +0300

    Writes a program that checks if last line fits requirements bu use of if else

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int a;
+
+	a = n % 10;
+	printf("Last digit of %d", n);
+	if (a > 5)
+	{
+		printf("is %d and is grater than 5", a);
+	}
+	else if (n == 0)
+	{
+		printf("is %d and is 0", a);
+	}
+	else
+	{
+		printf("is %d and is less than 6 and not 0", a);
+	}
+	return (0);
+}
