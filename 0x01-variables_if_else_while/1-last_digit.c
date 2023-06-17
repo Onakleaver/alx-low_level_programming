16d3b23e40bafe81c10480c277873b2fba9ffd5a
Kleaver <KLeaver49@gmail.com>
2023-06-17 04:15:40 +0300

    Adds new lines after statment is printed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,16 @@ int main(void)
 	printf("Last digit of %d", n);
 	if (a > 5)
 	{
-		printf(" is %d and is grater than 5", a);
+		printf(" is %d and is grater than 5\n", a);
 	}
 	else if (n == 0)
 	{
-		printf(" is %d and is 0", a);
+		printf(" is %d and is 0\n", a);
 	}
 	else
 	{
-		printf(" is %d and is less than 6 and not 0", a);
+		printf(" is %d and is less than 6 and not 0\n", a);
 	}
+
 	return (0);
 }
