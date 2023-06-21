121c348efb108157f443e1ba46e905a9d63a7da7
Kleaver <KLeaver49@gmail.com>
2023-06-21 05:36:12 +0300

    Adds file 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * main -print _putchar
- *this prints _putchar followed by a new line
- * Return: Always 0
- */
-int main(void)
+* main -prints _putchar
+*
+* Return: Always 0
+*/
+int _putchar(char c){
+int main()
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,6 +17,7 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
+	
 	return (0);
+}
 }
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+int  _putchar(char c);
+int main();
+{
+	int _putchar(char c);
+	printf("%s\n, c");
+	
+	return (0);
+}
