91fe9798db116f7fcdb642775d4cba71b202d0cb
Kleaver <KLeaver49@gmail.com>
2023-06-21 18:42:36 +0300

    Corrects the syntax for the prototype

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * main -prints alphabet in lowercase
+ * print_alphabet -prints alphabet in lowercase
  *
  * Return: Always 0
  */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet;
-	_putchar("\n");
+	char c;
 
-	return (0);
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
