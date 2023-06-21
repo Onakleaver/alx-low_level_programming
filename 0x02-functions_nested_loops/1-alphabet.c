ffbc2200801d1c486217f2da55be7571a89c1672
Kleaver <kleaver49@gmail.com>
2023-06-21 05:33:00 -0700

    prints alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main -prints alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar(print_alphabet);
+	_putchar("\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+	while (latter <= 'z')
+	{
+		printf("%c", letter);
+		letter ++;
+	}
+}
+
+int main ()
+{
+	void print_alphabet(void);
+	return 0;
+}
