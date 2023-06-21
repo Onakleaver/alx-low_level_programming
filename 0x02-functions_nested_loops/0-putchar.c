99057f0309f32c35f9216281ea499deff2a359d1
Kleaver <kleaver49@gmail.com>
2023-06-21 05:12:41 -0700

    Corrects the error of undecleared variable

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,7 @@
 *
 * Return: Always 0
 */
-int _putchar(char c){
-int main()
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -17,7 +16,6 @@ int main()
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
+
 	return (0);
-}
 }
