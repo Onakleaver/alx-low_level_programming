6022b881ac2b89520587f3308706bc53179965c8
Kleaver <KLeaver49@gmail.com>
2023-06-21 19:44:55 +0300

    Prints the alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,14 @@
  */
 void print_alphabet_x10(void)
 {
-	char c;
+	char c, i;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(c);
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
