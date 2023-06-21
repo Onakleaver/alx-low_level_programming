83edc2dd6d494186007f20b647b7035a28a16e97
Kleaver <KLeaver49@gmail.com>
2023-06-21 19:37:16 +0300

    Crates a function that prints lowecase alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -prints the alphabet 10x in lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar('\n');
+	}
