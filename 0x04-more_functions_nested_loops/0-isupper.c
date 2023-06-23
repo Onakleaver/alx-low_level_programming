edc671cb879fdfcf30a9905c71a75b3d212c4070
Kleaver <kleaver49@gmail.com>
2023-06-23 01:37:01 -0700

    Creates a function that checs if c is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <ctyoe.h>
+
+
+int _isupper(int c)
+{
+	if (isupper c)
+		return 1;
+	else
+		return 0;
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MY_HEDER_FILE
+#define MY_HEDER_FILE
+
+int _isupper(int c);
+
+#endif
