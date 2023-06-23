44a9798e8aab0a1fbcd8d02a0c0ef1446c7d4167
Kleaver <kleaver49@gmail.com>
2023-06-23 02:13:26 -0700

    Adds comments to code to comply with betty system

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 
-
+/**
+ * _isupper -Checks if input characters are in upper case
+ *@c: Character to be tested
+ * Return: 1 when true and 0 when false
+ */
 int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }
