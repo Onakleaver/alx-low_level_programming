46c78b9fd85b3a1709d476a3e7f16af874b38f2f
Kleaver <kleaver49@gmail.com>
2023-06-23 05:54:27 -0700

    Creates a function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul -multiplies charcters from input
+ * @a: character to be tested
+ * @b: character to be tested
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
