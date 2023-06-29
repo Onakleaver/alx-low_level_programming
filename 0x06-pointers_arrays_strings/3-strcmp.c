0678f61a3deb79a14e3418e3819a9950d977c73e
Kleaver <KLeaver49@gmail.com>
2023-06-29 19:10:17 +0300

    Writes a programe that functions like strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compares two stringd
+ * @s1: first string to compare
+ * @s2: second string compared
+ * Return: return.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result = strcmp(s1, s2);
+	return (result);
+}
