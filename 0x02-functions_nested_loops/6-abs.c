907b9d1b850dc33ad84089170c0c226d3c046ab5
Kleaver <kleaver49@gmail.com>
2023-07-02 00:17:59 -0700

    amends the script

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - prints the absolute numbers
+ * @i: integer value
+ * Return: Always 0
+ */
+int _abs(int i)
+{
+	abs( i );
+	return (i);
+}
