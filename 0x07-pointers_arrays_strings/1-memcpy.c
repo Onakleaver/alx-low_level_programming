c5199e3ff34ee529a0cced5fe58c361d5128517e
Kleaver <kleaver49@gmail.com>
2023-07-04 09:13:54 -0700

    creates function like memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,17 @@
 #include "main.h"
 
 /**
+ * _memcpy - copies characters from one file to another location
+ * @dest: pointer to the destination file
+ * @src: data to be copied
+ * @n: size of data to copy
+ * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	while (n--)
+	{
+		dest = src;
+	}
+	return (dest);
+}
