4c674466be324f4f8a5c64c02eb4f4b98e1fb68f
Kleaver <kleaver49@gmail.com>
2023-07-04 05:07:44 -0700

    points *p to *s

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,9 +10,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	for (unsigned int i = 0; i < n; i++)
+	char *p = s;
+
+	while (n > 0)
 	{
-		s[i] = b;
+		*p = b;
+		p++;
+		n--;
 	}
 	return (s);
 }
