62df80fe310a080c2875d7a9d576340fa1984c0e
Kleaver <KLeaver49@gmail.com>
2023-07-08 04:07:46 +0300

    Creates program that prints entered commans

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: prints number of arguments
+ * @argv: prints out the commands entered
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < argc; argc++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			_putchar(argv[i][j]);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
