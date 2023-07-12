20810821900a06d72c7cf139ce34d7f0b8cfaad1
Kleaver <KLeaver49@gmail.com>
2023-07-12 06:35:36 +0300

    Programe crates an array from malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,10 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * create_array - creates an array
+/**
+ * create_array - creates a pointer to an array
  * @size: size to be allocated
  * @c: character to be stored
  * Return: nothing
  */
 char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	char *array;
+
+	array = malloc(size * sizeof(char));
+
+	if (array != NULL)
+	{
+		for (unsinged int i = 0; i < size; i++)
+		{
+			array[i] = c;
+		}
+	}
+	return (array);
+}
