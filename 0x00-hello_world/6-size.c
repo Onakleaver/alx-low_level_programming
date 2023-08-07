217aa9c3b5fd10e75d7c99aa3c41db0cf2856a0a
Kleaver <KLeaver49@gmail.com>
2023-08-07 21:03:21 +0300

    Follows betty style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	char d;
 	float f;
 
-	puts("Size of a char: %lu byte(s)",(unsigned long)sizeof(d));
-	puts("Size of an int: %lu byte(s)",(unsigned long)sizeof(a));
-	puts("Size of a long int: %lu byte(s)",(unsigned long)sizeof(b));
-	puts("Size of a long long int: %lu bytes(s)",(unsigned long)sizeof(c));
-	puts("Size of a float: %lu byte(s)",(unsigned long)sizeof(f));
+	puts("Size of a char: %lu byte(s)", (unsigned long)sizeof(d));
+	puts("Size of an int: %lu byte(s)", (unsigned long)sizeof(a));
+	puts("Size of a long int: %lu byte(s)", (unsigned long)sizeof(b));
+	puts("Size of a long long int: %lu bytes(s)", (unsigned long)sizeof(c));
+	puts("Size of a float: %lu byte(s)", (unsigned long)sizeof(f));
 	return (0);
 }
