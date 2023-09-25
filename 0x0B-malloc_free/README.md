9fa142baf94613e3408d1c52865781aaaf1d6e4a
Kleaver Onani <122821288+Onakleaver@users.noreply.github.com>
2023-09-25 05:44:28 +0300

    Delete 0x0B-malloc_free directory restart from deffering

--- 0x0B-malloc_free/README.md
+++
@@ -1,1 +0,0 @@
-This directory handles dynamic memory allocation by the use of malloc and free.
--- 0x0B-malloc_free/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MY_HEDDER_FILE
-#define MY_HEDDER_FILE
-
-int _putchar(char c);
-char *create_array(unsigned int size, char c);
-
-#endif
