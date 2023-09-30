d4085ff75eefadf3932c2e3b6d8f1bfbe87d5512
Kleaver <KLeaver49@gmail.com>
2023-09-30 05:51:15 +0300

    Prints result of multiplication of two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: prints number of arguments
+ * @argv: prints out the commands entered
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int k;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	k = i * j;
+	printf("%d\n", k);
+	return (0);
+}
