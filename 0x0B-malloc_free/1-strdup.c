f8a801a892efe20783dc5eed7c5d5e5818334243
Kleaver <KLeaver49@gmail.com>
2023-10-04 21:10:10 +0300

    Creates a function that works like strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * _strdup -duplicates strings into other pointers
+  * @str: string to created as duplicate
+  * Return: pointer to copied string
+  */
+char *_strdup(char *str)
+{
+	char *ptr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	ptr = malloc(sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(ptr, str);
+	return (ptr);
+}
