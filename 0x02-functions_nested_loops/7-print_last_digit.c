528bff5f99447f6757db7dcf0bb016036350d0a5
Kleaver <KLeaver49@gmail.com>
2023-10-07 16:03:41 +0300

    Prints last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_last_digit -prints last digit of a number
+  * @num: format of test number
+  * Return: always 0
+  */
+int print_last_digit(int num)
+{
+	int dig;
+
+	/*Converts the value of num into positive if negative*/
+	if (num < 0)
+	{
+		num = -num;
+	}
+	dig = num % 10;
+	_putchar('0' + dig);
+	return (dig);
+}
