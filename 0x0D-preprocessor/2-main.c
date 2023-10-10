f7e436cf6083571342aa34a6ac9b6e03c0f6633a
Kleaver <KLeaver49@gmail.com>
2023-10-10 04:21:57 +0300

    Uses the stdio.h

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,37 +1,7 @@
-#include "main.h"
-#include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-
-/**
-  * main -prints name of copiled file
-  * Return: 0
-  */
 int main(void)
 {
-	char *name;
-	int i;
-
-#define NAME __FILE__
-
-	name = NAME;
-	while (name[i] != '\0')
-	{
-		_putchar(name[i]);
-		i++;
-	}
-	_putchar('\n');
+	printf("%s\n", __FILE__);
 	return (0);
 }
