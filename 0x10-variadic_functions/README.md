802e1c48b6884e7e63b52f2f309d41de9f860e93
Kleaver Onani <122821288+Onakleaver@users.noreply.github.com>
2023-11-14 18:42:18 +0300

    Delete 0x10-variadic_functions directory

--- 0x10-variadic_functions/README.md
+++
@@ -1,1 +0,0 @@
-# Vardic fanctions 
--- 0x10-variadic_functions/variadic_functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef VARDIC_FUNCTIONS_H
-#define VARDIC_FUNCTIONS_H
-
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-int _putchar(char c);
-
-#endif
