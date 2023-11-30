8486ce6de2044242f5b17564ec65d9915b43cc37
Kleaver <KLeaver49@gmail.com>
2023-11-30 20:13:15 +0300

    Updates added

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _strdup- duplicates a string and stores it in a pointer
+* @str: string to be duplicated
+* Return: pointer to a duplicate string
+*/
+char *_strdup(char *
