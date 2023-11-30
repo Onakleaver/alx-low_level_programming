c77d391edfecdc7be653f9e228855b885f6bd35f
Kleaver <KLeaver49@gmail.com>
2023-11-30 20:22:16 +0300

    Counts every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-<<<<<<< HEAD
 /**
   * jack_bauer- prints every minuite of the day
   * Return: Nothing
@@ -24,37 +23,6 @@ void jack_bauer(void)
 					_putchar(c);
 					_putchar(d);
 					_putchar('\n');
-=======
-
-void jack_bauer(void)
-{
-	int a, b, c, d, e;
-
-	a = 48;
-	b = 48;
-	c = 58;
-	d = 48;
-	e = 48;
-
-	for (a = 48; a <= 50; a++)
-	{
-		_putchar(a);
-		a++;
-		while (b <= 51)
-		{
-			_putchar(b);
-			_putchar(c);
-			b++;
-			while (d <= 53)
-			{
-				_putchar(d);
-				d++;
-				while (e <= 57)
-				{
-					_putchar(e);
-					_putchar('\n');
-					e++;
->>>>>>> 1c20753970dd42143fca6f5e2c4c6bfd7d0ebcad
 				}
 			}
 		}
