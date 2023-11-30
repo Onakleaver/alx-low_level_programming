01ffaf6cdf649e0bb25641c2c2f4fa4bb72e777f
Kleaver <KLeaver49@gmail.com>
2023-11-30 21:34:28 +0300

    Reduces the variables

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,30 +1,23 @@
 #include "main.h"
 
 /**
-  * jack_bauer- prints every minuite of the day
-  * Return: Nothing
-  */
+* jack_bauer- prints every minuite of the day
+* Return: Nothing
+*/
 void jack_bauer(void)
 {
-	int a, b, c, d, f;
+	int a, c;
 
-	f = 58;
-	for (a = 48; a <= 50; a++)
+	for (a = 0; a <= 24; a++)
 	{
-		for (b = 48; b <= 51; b++)
+		for (c = 0; c < 60; c++)
 		{
-			for (c = 48; c <= 53; c++)
-			{
-				for (d = 48; d <= 57; d++)
-				{
-					_putchar(a);
-					_putchar(b);
-					_putchar(f);
-					_putchar(c);
-					_putchar(d);
-					_putchar('\n');
-				}
-			}
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
