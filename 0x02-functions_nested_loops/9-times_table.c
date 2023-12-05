9a422fa80e5ef21a66d1613c84015ea235de1bd4
Kleaver <kleaver49@gmail.com>
2023-12-05 08:49:38 -0800

    Prints times tables

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table -prints times tables from 0
+ * Return: Nothing
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+
+	a = 0;
+	b = 0;
+	while (a <= 9)
+	{
+		b = 0;
+		while (b <= 9)
+		{	
+			c = a * b;
+			if (c >= 10)
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			_putchar(',');
+			b++;
+		}
+		a++;
+		_putchar('\n');
+	}
+}
