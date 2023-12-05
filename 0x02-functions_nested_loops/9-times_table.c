052c785e367d28136ec0bb47ce6103ddbc1f930a
Kleaver <kleaver49@gmail.com>
2023-12-05 10:28:19 -0800

    Works on two spaces

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,6 @@ void times_table(void)
 			{
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
-				_putchar(' ');
 			}
 			else
 			{
@@ -31,6 +30,7 @@ void times_table(void)
 			if (b < 9)
 			{
 				_putchar(',');
+				_putchar(' ');
 			}
 			b++;
 		}
