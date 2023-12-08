621a4b3ccff448c54b70dac5c1f941277cff3be3
Kleaver <KLeaver49@gmail.com>
2023-12-08 20:52:00 +0300

    Function adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - Adds two integers
+  * @a: integers to be added
+  * @b: integers to be added
+  * Return: Value of addition of two integers
+  */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
