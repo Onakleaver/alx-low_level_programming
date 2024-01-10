6f8c74f597f264ca94c2b4c21c8b443aa3eb9236
Kleaver Onani <122821288+Onakleaver@users.noreply.github.com>
2024-01-10 06:40:40 +0300

    Delete 0x0B-malloc_free directory

--- 0x0B-malloc_free/1-strdup.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-#include <stdlib.h>
-
-/**
-* _strdup- duplicates a string and stores it in a pointer
-* @str: string to be duplicated
-* Return: pointer to a duplicate string
-*/
-char *_strdup(char *
