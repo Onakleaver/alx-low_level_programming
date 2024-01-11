c3a634a2d7f72e76eac5d2a2561c42a56204b0fa
Kleaver <KLeaver49@gmail.com>
2024-01-11 05:54:09 +0300

    Gets prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* is_prime_number -checks if number is a prime number
+* @n: test number
+* Return: 1 if is prime and 0 if not
+*/
+int is_prime_number(int n)
+{
+	int i;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n == 2)
+	{
+		return (1);
+	}
+	else if (n % i == 0 && i > 0)
+	{
+		return (0);
+	}
+	else if (i == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		i = i - 1;
+		return (is_prime_number(n));
+	}
+}	
