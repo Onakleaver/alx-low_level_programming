42dee5a378358adb4252a31dd8710fdb6b1c0325
Kleaver <KLeaver49@gmail.com>
2024-02-03 16:08:04 +0300

    Prints out the cmmandline arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main -code entry point
+  * @argc: argument count
+  * @argv: argument vector
+  * Return: always 0
+  */
+int main(int argc, char **argv)
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argv[i]);
+		printf("\n");
+	}
+
+	return (0);
+}
