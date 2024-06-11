000384c0a32de9002a0e19f47a0aae6c8df54ee2
Kleaver <kleaver49@gmail.com>
2024-06-11 21:49:18 +0300

    Add arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -code entry point
+ *
+ * @argc: argument count
+ * @argv: argument variable
+ *
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	int a;
+	int sum;
+	char *endptr;
+	int num;
+
+	(void) argc;
+	sum = 0;
+
+	for (a = 1; a < argc; a++)
+	{
+		num = strtol(argv[a], &endptr, 10);
+
+	if (*endptr != '\0')
+	{
+	printf("Error \n");
+
+	return (1);
+	}
+
+	if (num == 0)
+	{
+		printf("0 \n");
+	}
+	}
+
+	sum += num;
+	printf("%d\n", sum);
+
+	return (0);
+}
