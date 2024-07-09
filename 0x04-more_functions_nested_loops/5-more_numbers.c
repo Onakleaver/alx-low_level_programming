8ba521badc8bd3b4d34bae66b9dec6064c43c330
Kleaver <kleaver49@gmail.com>
2024-07-08 22:49:36 -0700

    code update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,26 +9,20 @@ void more_numbers(void)
 {
 	int a;
 	int b;
-	int c;
 
-	a = 0;
-	c = 0;
-	while (a < 10)
+	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			if (b <= 9)
+			if (b > 9)
 			{
-				_putchar(b + '0');
+				_putchar((b / 10) + '0');
 			}
 			else
 			{
-				c = b % 10;
-				_putchar('1');
-				_putchar(c + '0');
+				_putchar((b % 10) + '0');
 			}
 		}
-		_putchar ('\n');
-		a++;
+		_putchar('\n');
 	}
 }
