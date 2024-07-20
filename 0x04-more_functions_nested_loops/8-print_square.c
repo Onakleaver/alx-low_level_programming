88c8bcbef2ba2924e9334e6586fb0857e75b6796
Kleaver <kleaver49@gmail.com>
2024-07-20 15:38:14 +0300

    Print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square -prints square
+ * @size: size of the square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int a;
+
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+	for (a = 0; a < size; a++)
+	{
+		_putchar('\#');
+	}
+}
