91d77fe6a914c7318f835c3f2bffd856b0612334
Kleaver <KLeaver49@gmail.com>
2024-07-23 12:09:22 +0300

    Checks for divisibility by both 5 & 3

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,21 +5,21 @@
  * Return: Always 0
  */
 
-int main()
+int main(void)
 {
 	int a;
 
-	for (a = 0; a <= 100; a++)
+	for (a = 1; a <= 100; a++)
 	{
-		if (a%3 == 0)
+		if (a % 3 && a % 5 == 0)
 		{
-			if (a%3 && a%5 == 0)
-			{
-				printf("FizzBuzz");
-			}
+			printf("FizzBuzz");
+		}
+		else if ( a % 3 == 0)
+		{
 			printf("Fizz");
 		}
-		else if (a%5 == 0)
+		else if (a % 5 == 0)
 		{
 			printf("Buzz");
 		}
