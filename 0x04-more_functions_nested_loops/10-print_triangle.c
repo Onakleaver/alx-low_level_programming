9e642714eaa259d19ff4824a0222ec31ae01b4e0
Kleaver <kleaver49@gmail.com>
2024-07-24 21:19:07 +0300

    Prints triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,4 +8,25 @@
 
 void print_triangle(int size)
 {
+	int a;
+	int b;
+	int c;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (a = 0; a < size; a++)
+	{
+		for (c = 0; c < size - a - 1; c++)
+		{
+			_putchar(' ');
+		}
+		for (b = 0; b <= a; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
