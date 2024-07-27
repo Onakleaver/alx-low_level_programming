c0c68eb5a417aacc5a7ad88a72e71dac37018fdf
root <root@KLEAVER>
2024-07-27 14:25:18 +0300

    Adds comments to the code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,14 @@
 void print_rev(char *s)
 {
 	int length = strlen(s);
+	/* gets the full length of the string */
 	int i;
 
 	for (i = length - 1; i >= 0; i--)
+		/*The loop starts from the last character in the string after the null */
 	{
 		_putchar(s[i]);
+		/* prints the string at the i'th position */
 	}
 	_putchar('\n');
 }
